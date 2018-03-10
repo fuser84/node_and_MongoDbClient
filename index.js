daa70f5b1d6d3a7dc8de669e73fc468ecd258794
fuser <vnovodid@gmail.com>
2018-03-10 10:37:27 +0200

    Callback Hell and Promises

--- index.js
+++ index.js
@@ -4,34 +4,38 @@ const dboper = require('./operations');
 
 const url = 'mongodb://localhost:27017/conFusion';
 
-MongoClient.connect(url, (err, client) => {
+MongoClient.connect(url)
+    .then((client) => {
+        console.log(`Connected correctly to the server`);
 
-    assert.equal(err, null); //check for no errors
-    console.log(`Connected correctly to the server`);
+        const db = client.db('conFusion');
 
-    const db = client.db('conFusion');
+        dboper.insertDocument(db, {name: "Vadonut", description: "Test"}, "dishes")
+            .then((result) => {
+                console.log(`Insert Document:\n`, result.ops);
 
-    dboper.insertDocument(db, {name: "Vadonut", description: "Test"}, "dishes", (result) => {
-        console.log(`Insert Document:\n`, result.ops);
+                return dboper.findDocuments(db, "dishes");
+            })
+            .then((docs) => {
+                console.log(`Found Documents:\n`, docs);
 
-        dboper.findDocuments(db, "dishes", (docs) => {
-            console.log(`Found Documents:\n`, docs);
+                return dboper.updateDocument(db, {name: "Vadonut"}, {description: "Updated Test"},
+                    "dishes");
+            })
+            .then((result) => {
+                console.log(`Updated document:\n`, result.result);
 
-            dboper.updateDocument(db, {name: "Vadonut"}, {description: "Updated Test"},
-                "dishes", (result) => {
-                    console.log(`Updated document:\n`, result.result);
-
-                    dboper.findDocuments(db, "dishes", (docs) => {
-                        console.log(`Found Updated Documents:\n`, docs);
-
-                        db.dropCollection("dishes", (result) => {
-                            console.log(`Dropped Collection: `, result);
-
-                            client.close();
-                        });
-                    });
-                });
-        });
-    });
-});
+                return dboper.findDocuments(db, "dishes");
+            })
+            .then ((docs) => {
+            console.log(`Found Updated Documents:\n`, docs);
+            return db.dropCollection("dishes")
+            })
+            .then ((result) => {
+                console.log(`Dropped Collection: `, result);
+                return client.close();
+            })
+            .catch((err) => console.log(err));
+}, (err) => console.log(err))
+.catch((err) => console.log(err));//catch all possible specific errors if onReject function won't cope with it in promise
 
